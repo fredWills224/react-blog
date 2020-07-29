95aa73f6ac255ed065109cde000dfefee93d987b
fredWills422 <willsf422@gmail.com>
2020-07-29 06:14:49 -0400

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://willsf:Harmony2006!@react-blog.hzgol.mongodb.net/react-blog-db?retryWrites=true&w=majority',
-    {useNewUrlParser: true}).then(() => console.log('DB connected'))
-                             .catch(err => console.error(err));
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,
+{useNewUrlParser: true}).then(() => console.log('DB connected'))
+                        .catch(err => console.error(err));
 
 app.get('/', (req, res) =>{
     res.send('hello world')
 });
 
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.listen(5000)
+app.post('/api/users/register', (req, res) =>{
+    
+    const user = new User(req.body)
+    user.save((err, userData) => {
+        if(err) return res.json ({success: false, err})
+    })
+    return res.status(200).json({
+        success:true
+    })
+
+});
+
+app.listen(5000);
 
