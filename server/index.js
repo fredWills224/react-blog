79e84918eed9f203f29c0ae4b862030f22d7381b
fredWills422 <willsf422@gmail.com>
2020-07-31 10:32:32 -0400

    heroku again1

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.json({"hello": "Hello World"});
+app.get("/", (req, res) => {
+    res.json({"hello": "Hello World"})
 });
 
 app.get('/api/user/auth', auth, (req, res) =>{
