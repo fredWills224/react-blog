6b63aef5423c7c76ea019dbe2ce6bcdef6e9a10d
fredWills422 <willsf422@gmail.com>
2020-08-02 12:23:02 -0400

    login page checkpoint

--- client/src/components/RegisterLogin/index.js
+++ client/src/components/RegisterLogin/index.js
@@ -1,22 +1,80 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/user_actions'
 
 class RegisterLogin extends Component {
+    
+    state = {
+        email: "",
+        password: "",
+        errors: [""]
+    }
+
+    displayErrors = errors => 
+        errors.map((error, i) => <p key={i} >{error}</p>)
+    ;
+    
+
+    handleChange = event =>{
+
+        this.setState({ [event.target.name] : event.target.value });
+
+    }
+
+    submitForm = event => {
+
+        event.preventDefault();
+        let dataToSubmit = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        if(this.isFormValid(this.state)){
+            this.setState({ errors: [] })
+
+                this.props.dispatch(loginUser(dataToSubmit))
+                    .then(response => {
+
+                        if (response.payload.loginSuccess){
+                            this.props.history.push('/');
+                        } else {
+                            this.setState({
+                                errors: this.state.errors.concat(
+                                    "Failed to log in, check email and password"
+                                )
+                            })
+                        }
+
+                    })
+                ;
+                
+            ;
+        } else {
+            this.setState({
+                errors: this.state.errors.concat('Form is not valid')
+            });
+        }
+
+    }
+
+    isFormValid = ({ email, password }) => email && password;
+
     render() {
         return (
             <div className="container">
                 <h2> Login </h2>
 
                 <div className="row">
-                {/* onSubmit={event => this.submitForm(event)} */}
-                    <form className="col s12">
+                
+                    <form className="col s12" onSubmit={event => this.submitForm(event)}>
                         
                         <div className="row">
                             <div className="input-field col s12">
 
                                 <input
                                     name="email"
-                                    // value={this.state.email}
-                                    // onChange={e => this.handleChange(e)}
+                                    value={this.state.email}
+                                    onChange={e => this.handleChange(e)}
                                     id="email"
                                     type="email"
                                     // className from materilize css
@@ -38,8 +96,8 @@ class RegisterLogin extends Component {
 
                                 <input
                                     name="password"
-                                    // value={this.state.password}
-                                    // onChange={e => this.handleChange(e)}
+                                    value={this.state.password}
+                                    onChange={e => this.handleChange(e)}
                                     id="password"
                                     type="password"
                                     // className from materilize css
@@ -55,6 +113,12 @@ class RegisterLogin extends Component {
 
                             </div>
                         </div>
+
+                        {this.state.errors.length >0 && (
+                            <div>
+                                {this.displayErrors(this.state.errors)}
+                            </div>
+                        )}
 
                         <div className="row">
                             <div className="col s12">
@@ -79,4 +143,10 @@ class RegisterLogin extends Component {
     }
 }
 
-export default RegisterLogin;
+function mapStateToProps(state){
+    return{
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps) (RegisterLogin);
--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,6 +1,15 @@
+import{
+    LOGIN_USER
+} from '../actions/types';
+
 export default function(state={}, action){
 
     switch(action.type){
+        case LOGIN_USER:
+            //(...)state = everything from state
+            return {...state, loginSuccess: action.payload}
+        ;
+
         default:
             return state
         ;
